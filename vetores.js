122d4bd86c0f04782075504179cdae37d2558ad0
Prof Edir <ecaramba@gmauil.com>
2024-11-27 20:56:18 -0300

    lista

---
+++ vetores.js
@@ -0,0 +1,51 @@
+// estrutura de dados
+        // vetores -> array
+
+        var cidade = ["Curitiba", "Colombo", "Pinhais"];
+
+        // retorna a cidade da posição
+        cidade[1];
+
+        //adiciona item no final
+        cidade.push("Araucaria");
+
+        console.log(cidade);
+
+        // loops -> laço de repetição
+        var ini = 1;
+        var fim = 10;
+
+        while (ini <= fim)
+        {
+            console.log(ini);
+            ini = ini + 1; 
+        }
+
+        // invertido
+        var ini = 10;
+        var fim = 1;
+
+        while (ini >= fim)
+        {
+            console.log(ini);
+            ini = ini - 1; 
+        }
+
+
+        function listar()
+        {
+            var ini = 0;
+            var fim = cidade.length - 1;
+
+            var res = "<ul>";
+
+            while(ini <= fim)
+            {
+                res = res + "<li>"+ cidade[ini] +"</li>";
+                ini = ini + 1;
+            }
+
+            res = res + "</ul>";
+            
+            resultado.innerHTML = res;
+        }
