66d5fe59535de7c986d41e51c8cf0deb5e591efa
Prof Edir <ecaramba@gmauil.com>
2024-11-27 21:58:10 -0300

    funcoes

---
+++ func.js
@@ -0,0 +1,23 @@
+
+// E -> P -> S
+
+var n1 = 4;
+var n2 = 6;
+var n3 = 2;
+
+function soma(n1, n2)
+{
+    var n4 = 1;
+    var res = n1 + n2 + n3 + n4
+    return res;
+}
+
+function sub(n1, n2)
+{
+    var res = n1 + n2 + n3 + n4
+    return res;
+}
+
+soma(2,3);
+
+console.log(n1);
