84b91e8781f24d55cb19473601c2411f84043a27
Prof Edir <ecaramba@gmauil.com>
2024-12-02 20:25:25 -0300

    regex

--- form.js
+++ form.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         $("#nome").removeClass("is-invalid");
         $("#cidade").removeClass("is-invalid");
         $("[name=sexo]").removeClass("is-invalid");
+        $("#cpf").removeClass("is-invalid");
 
         var nome = $("#nome").val();
         if (nome.trim() == "")
@@ -20,6 +21,22 @@ $(document).ready(function(){
         if ($("[name=sexo]:checked").length == 0)
         {
             $("[name=sexo]").addClass("is-invalid");
+        }
+
+        var cpf_regex = /^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$/gm;
+        var cpf = $("#cpf").val();
+
+        if(cpf_regex.test(cpf) == false)
+        {
+            $("#cpf").addClass("is-invalid");
+        }
+
+        var email_regex = /^[a-z0-9.\-_+]+@[a-z0-9.]+/gmi
+        var email = $("#email").val();
+
+        if (email_regex.test(email) == false)
+        {
+            $("#email").addClass("is-invalid");
         }
 
     }); // fim do bt
